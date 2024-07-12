aa9fd2bb174e3bed3594226e3fa07a45a00f8fd4
Tu Nombre <miguelalcaniz02@gmail.com>
2024-07-12 11:20:00 +0200

    OptOpt defined functions in the cpp file

--- src/OptOpt.cpp
+++ src/OptOpt.cpp
@@ -1,6 +1,39 @@
 #include "OptOpt.hpp"
 #include <vector>
 
+
+//Method for initializing the vector with size L
+void sign_scheme::init_scheme(const std::vector<int>& states_values,const size_t A_value)
+{   
+  L = states_values.size();
+  states = states_values;
+  A = A_value;
+
+  squeme.resize(L);
+
+  for(int l = 0; l < L; ++l){
+    squeme[l].resize(states[l]);
+    for(int s = 0; s < states[l]; ++s){
+      squeme[l][s].resize(A);
+      for(int k = 0; k < A; ++k)
+        squeme[l][s][k].resize(A);
+    }
+  }
+}
+
+// Method for getting the recommendation out of the probability distribution
+int sign_scheme::recommendation(int l, int s, int outcome)
+{      
+  std::random_device re;
+  std::knuth_b knuth(re());
+ 
+  probs &prob = squeme[l][s][outcome];
+  std::discrete_distribution<> distribution(prob.begin(), prob.end());
+  int rec = distribution(knuth);
+  return rec;
+}
+
+
 /*
 This function OptOpt is supposed to solve a maximitation problem called Optimistic Optimitation problem (Opt-Opt)
 */
--- src/OptOpt.hpp
+++ src/OptOpt.hpp
@@ -15,44 +15,19 @@ using state_outcome_probs = std::vector<std::vector<outcome_probs>>;
 class sign_scheme{
 public:
 
-  //Constructor
+  // Constructor
   sign_scheme(){};
 
-  //Method for initializing the vector with size L
-  void init_scheme(const std::vector<int>& states_values,const size_t A_value){
-    
-    L = states_values.size();
-    states = states_values;
-    A = A_value;
+  // Method for initializing the vector with size L
+  void init_scheme(const std::vector<int>& states_values,const size_t A_value);
 
-    squeme.resize(L);
-
-    for(int l = 0; l < L; ++l){
-      squeme[l].resize(states[l]);
-      for(int s = 0; s < states[l]; ++s){
-          squeme[l][s].resize(A);
-          for(int k = 0; k < A; ++k){
-              squeme[l][s][k].resize(A);
-          }
-      }
-    }
-  }
-
+  // Method for getting the values
   double get_sign(int l, int s, int k, int a_value){
     return squeme[l][s][k][a_value];
   }
 
-
-  int recommendation(int l, int s, int outcome)
-  {      
-    std::random_device re;
-    std::knuth_b knuth(re());
-   
-    probs &prob = squeme[l][s][outcome];
-    std::discrete_distribution<> distribution(prob.begin(), prob.end());
-    int rec = distribution(knuth);
-    return rec;
-  }
+  // Method for getting the recommendation out of the probability distribution
+  int recommendation(int l, int s, int outcome);
 
   // Declaration of Optimistic optimitation problem 
   friend void OptOpt(sign_scheme &SQ);
